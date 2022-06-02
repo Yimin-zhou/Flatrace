e898b0b706e3d0d10383badf305782df9c768400
Wouter Bijlsma <wouter@wouterbijlsma.nl>
2022-06-02 23:29:55 +0200

    Main test program now correctly ray-traces a single triangle
    And saves it to a PPM file for visual verification

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include "core/types.h"
 #include "core/intersect.h"
+#include "core/frame.h"
 
-#include <vector>
+#include "utils/ppm.h"
+
 #include <chrono>
 #include <iostream>
 #include <iomanip>
@@ -10,31 +12,43 @@ using namespace core;
 
 int main(int argc, char **argv)
 {
-  constexpr auto N_RAYS_BITS = 8;
-  constexpr auto N_RAYS = (1 << N_RAYS_BITS);
-  constexpr auto N = 1'000'000'000;
+  constexpr auto FRAME_WIDTH = 1024;
+  constexpr auto FRAME_HEIGHT = 768;
+
+  constexpr float DX = 1.0f / FRAME_WIDTH;
+  constexpr float DY = 1.0f / FRAME_HEIGHT;
+
+  constexpr auto N_FRAMES = 1;
+  constexpr auto N_RAYS = N_FRAMES * FRAME_WIDTH * FRAME_HEIGHT;
 
   using namespace std::chrono;
 
-  const Triangle triangle({ 0.0f, -0.5f, 1.0f }, { -0.5f, 0.5f, 1.0f }, { 0.5f, 0.5f, 1.0f });
+//  const Triangle triangle({ 0.0f, -0.5f, 1.0f }, { -0.5f, 0.5f, 1.0f }, { 0.5f, 0.5f, 1.0f });
+  const Triangle triangle({ 0.0f, -0.1f, 1.0f }, { -0.1f, 0.1f, 1.0f }, { 0.1f, 0.1f, 1.0f });
 
-  std::vector<Ray> rays(N_RAYS);
-
-  for (auto i = 0; i < N_RAYS; i++)
-  {
-    const float x = ((float) rand() / RAND_MAX) - 0.5f;
-    const float y = ((float) rand() / RAND_MAX) - 0.5f;
-
-    rays[i] = { { x, y, 0.0f }, { 0.0f, 0.0f, 1.0f } };
-  }
+  Frame frame(1024, 768);
 
   const auto start = steady_clock::now();
 
   int n_hit = 0;
 
-  for (size_t i = 0; i < N; i++)
+  for (int i = 0; i < FRAME_HEIGHT; i++)
   {
-    n_hit += intersect(triangle, rays[i & (N_RAYS - 1)]);
+    const float y = -0.5f + (i * DY);
+    float x = -0.5f;
+
+    RGBA *p = frame.pixels.get() + i*FRAME_WIDTH;
+
+    for (int j = 0; j < FRAME_WIDTH; j++)
+    {
+      const bool hit = intersect(triangle, { { x, y, 0.0f }, { 0.0f, 0.0f, 1.0f } });
+
+      *p = (hit ? RGBA{ 255, 255, 255, 255 } : RGBA{ 0, 0, 0, 255 });
+
+      n_hit += hit;
+      x += DX;
+      p += 1;
+    }
   }
 
   const auto end = steady_clock::now();
@@ -42,9 +56,10 @@ int main(int argc, char **argv)
   std::cerr << "N HIT: " << n_hit << std::endl;
 
   std::cerr << std::setprecision(12)
-            << N << " intersections took: " << duration_cast<milliseconds>(end - start).count() << "ms ("
-            << N * (1'000'000.0 / duration_cast<microseconds>(end - start).count()) << " rays/second)" << std::endl;
+            << N_RAYS << " intersections took: " << duration_cast<milliseconds>(end - start).count() << "ms ("
+            << N_RAYS * (1'000'000.0 / duration_cast<microseconds>(end - start).count()) << " rays/second)" << std::endl;
 
+  utils::Ppm::write("out.ppm", frame);
 
   return EXIT_SUCCESS;
 }
