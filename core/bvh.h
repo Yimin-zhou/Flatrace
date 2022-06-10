0cc9da2b7bf971102e52c48c815c83a7ae429a27
Wouter Bijlsma <wouter@wouterbijlsma.nl>
2022-06-10 18:09:51 +0200

    Implement front-to-back BVH traversal
    While traversing the BVH, child nodes to recurse into are now processed in
    front-to-back order.
    
    Unfortunately for the bunny test, for the camera view we have been using so far
    this does not improve render performance, in fact it increases render time by ~15%.
    This can be explained by the fact that the bunny model has very little 'overdraw'
    no matter which order you traverse it in, it is almost a convex model, and except
    for rays hitting its ears pretty much all rays will hit the model exactly once.
    
    The front-to-back traversal does have a very large effect on render time of models
    that would have significant overdraw when rendered with a traditional back-to-front
    rasterizer (painters algorithm). For example rendering the rear-view camera point for
    the house scene takes exactly the same time as the front-view camera point when
    using ordered traversal, while unordered/arbitrary traversal the rear-view camera
    point takes twice as long to render. For this reason, we keep the ordered traversal.

--- core/bvh.h
+++ core/bvh.h
@@ -1,3 +1,7 @@
+// Implements a bounding volume hierarchy class for accelerated ray/triangle intersections.
+//
+// This code is based on the article 'How to build a BVH - part 2' by Jacco Bikker
+
 #pragma once
 
 #include "types.h"
@@ -18,25 +22,48 @@ class BVH
       Node *node_stack[_depth];
       int stack_pointer = 0;
 
-      node_stack[stack_pointer++] = _root;
+      if (core::intersect(_root->bbox, ray) != INF)
+      {
+        node_stack[stack_pointer++] = _root;
+      }
 
+      // Traversal works like this: while there are nodes left on the stack, pop the topmost one. If it is a leaf,
+      // intersect & shorten the ray against the triangles in the leaf node. If the node is an internal node,
+      // intersect the ray against its left & right child node bboxes, and push those child nodes that were hit,
+      // ordered by hit distance, to ensure the closest node gets traversed first.
       while (stack_pointer != 0)
       {
         Node * const node = node_stack[--stack_pointer];
 
-        if (node && core::intersect(node->bbox, ray))
+        if (node->isLeaf)
         {
-          if (node->isLeaf)
+          for (int i = node->from; i < node->to; i++)
           {
-            for (int i = node->from; i < node->to; i++)
-            {
-              core::intersect(getTriangle(i), ray);
-            }
+            core::intersect(getTriangle(i), ray);
           }
-          else
+        }
+        else
+        {
+          Node *left = node->left;
+          Node *right = node->right;
+
+          float t_left = core::intersect(left->bbox, ray);
+          float t_right = core::intersect(right->bbox, ray);
+
+          if (t_left > t_right)
           {
-            node_stack[stack_pointer++] = node->left;
-            node_stack[stack_pointer++] = node->right;
+            std::swap(t_left, t_right);
+            std::swap(left, right);
+          }
+
+          if (t_left != INF)
+          {
+            if (t_right != INF)
+            {
+              node_stack[stack_pointer++] = right;
+            }
+
+            node_stack[stack_pointer++] = left;
           }
         }
       }
